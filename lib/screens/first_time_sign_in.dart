411f6018b2e433899d1490b767e036142ff740ce
CGroff <63170672+Slmply@users.noreply.github.com>
2025-01-26 15:18:43 -0600

    Finished Main Onboarding

--- lib/screens/first_time_sign_in.dart
+++ lib/screens/first_time_sign_in.dart
@@ -1,21 +1,113 @@
-
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fbla_application/utils/constants.dart';
+import 'package:fbla_application/utils/global_widgets.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class FirstTimeSignIn extends StatelessWidget{
-  FirstTimeSignIn({super.key});
+class FirstTimeSignIn extends StatefulWidget{
+  const FirstTimeSignIn({super.key});
+
+  @override
+  State createState() => _FirstTimeSignInState();
+}
+
+enum EducatorType {
+  student, teacher
+}
+
+class _FirstTimeSignInState extends State<FirstTimeSignIn>{
 
   final firstNameController = TextEditingController();
   final lastNameController = TextEditingController();
+
+  final PageController pageController = PageController();
+
+  final _nameFormKey = GlobalKey<FormState>();
+
+  EducatorType? selectedType;
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    firstNameController.dispose();
+    lastNameController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Setup')),
-      body: buildNameScreen(context),
+      body: PageView(
+        controller: pageController,
+        children: [
+          buildNameScreen(context),
+          buildUserTypeSelectionScreen(context),
+        ]
+      ),
+    );
+  }
+
+  Center buildUserTypeSelectionScreen(BuildContext context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 20),
+          Text('I am a:', style: Theme.of(context).textTheme.displaySmall),
+          const SizedBox(height: 20),
+          ToggleButtons(
+            isSelected: [
+              selectedType == EducatorType.student,
+              selectedType == EducatorType.teacher,
+            ],
+            onPressed: (index) {
+              setState(() {
+                selectedType = EducatorType.values[index];
+              });
+            },
+            children: const [
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text('Student'),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text('Teacher'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  pageController.animateToPage(0, duration: Duration(milliseconds: 750), curve: Curves.easeInOutCubic);
+                },
+                child: const Text('Back'),
+              ),
+              const SizedBox(width: 20),
+              ElevatedButton(
+                onPressed: (selectedType != null) ? () {
+                  final db = FirebaseFirestore.instance;
+                  db.collection('users').doc(FirebaseAuth.instance.currentUser?.uid).set({
+                    'User First': firstNameController.text,
+                    'User Last': lastNameController.text,
+                    'User Type': selectedType?.index,
+                  }).then((value) {
+                    Navigator.pushReplacementNamed(context, Constants.homeRoute);
+                  }).onError((error, stackTrace) {
+                    GlobalWidgets(context).showSnackBar(content: "Error Creating User!", backgroundColor: Colors.red);
+                  });
+                } : null,
+                child: const Text('Next'),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -31,41 +123,58 @@ class FirstTimeSignIn extends StatelessWidget{
           const SizedBox(height: 20),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              // mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  child: TextFormField(
-                    controller: firstNameController,
-                    decoration: const InputDecoration(
-                      labelText: 'First Name',
-                      border: OutlineInputBorder(),
+            child: Form(
+              key: _nameFormKey,
+              child: Row(
+                // mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      controller: firstNameController,
+                      decoration: const InputDecoration(
+                        labelText: 'First Name',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your first name.';
+                        } else if (value.contains(RegExp('[^a-zA-Z]'))) {
+                          return 'Please enter a valid name.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ),
-                const SizedBox(width: 20),
-                Expanded(
-                  child: TextFormField(
-                    controller: lastNameController,
-                    decoration: const InputDecoration(
-                      labelText: 'Last Name',
-                      border: OutlineInputBorder(),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: TextFormField(
+                      controller: lastNameController,
+                      decoration: const InputDecoration(
+                        labelText: 'Last Name',
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter your last name.';
+                        } else if (value.contains(RegExp('[^a-zA-Z]'))) {
+                          return 'Please enter a valid name.';
+                        }
+                        return null;
+                      },
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           const SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
-              final db = FirebaseFirestore.instance;
-
-              db.collection('users').doc(FirebaseAuth.instance.currentUser?.uid).set({
-                'User First': firstNameController.text,
-                'User Last': lastNameController.text,
-              });
-              Navigator.pushReplacementNamed(context, Constants.homeRoute);
+              if (_nameFormKey.currentState!.validate()) {
+                pageController.animateToPage(1, duration: Duration(milliseconds: 750), curve: Curves.easeInOutCubic);
+              } else {
+                GlobalWidgets(context).showSnackBar(content: "Please Enter Your Name", backgroundColor: Colors.red);
+              }
             },
             child: const Text('Next'),
           ),
