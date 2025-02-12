d95c9d81ae41a917c63e558da14d75bc322ecf66
Casey Groff <63170672+Slmply@users.noreply.github.com>
2025-02-12 14:53:01 -0600

    added class home screen

---
+++ lib/screens/class_home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class classHome extends StatefulWidget {
+    const classHome({super.key});
+
+    @override
+    _classHomeState createState() => _classHomeState();
+}
+
+class _classHomeState extends State<classHome> {
+
+    @override
+    Widget build (BuildContext context) {
+
+        return null;
+    } 
+}
