c8af8a3273b92539d9adeb74dce47e8f04b943fc
Aiden Davis <aiden.davis@stwwhitefishbay.org>
2025-02-24 21:28:57 -0600

    gay shit

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -6,7 +6,7 @@ class Constants {
 
   // Routes
   static const String homeRoute = "/home";
-  static const String signInRoute = "/home/sign-in";
+  static const String signInRoute = "/sign-in";
   static const String firstTimeSignInRoute = "/first-time-sign-in";
   static const String profileRoute = "/profile";
   static const String landingRoute = "/landing";
